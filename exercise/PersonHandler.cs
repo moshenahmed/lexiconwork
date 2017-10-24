017fd4a6e55c040d8d6f7050f9c283e24e41f924
moshenahmed@gmail.com <moshenahmed@gmail.com>
2017-10-24 17:10:54 +0200

    final code for the day

--- exercise/PersonHandler.cs
+++ exercise/PersonHandler.cs
@@ -6,43 +6,23 @@ using System.Threading.Tasks;
 
 namespace exercise
 {
-      class PersonHandler 
+    class PersonHandler
     {
         public Person CreatePerson(int age, string fname, string lname, double height, double weight)
         {
             Person people = new Person();
-            age = people.Age ;
-            fname = people.FName;
-            lname = people.LName;
-            height = people.Height;
-            weight = people.Weight;
+            people.Age = age;
+            people.FName = fname;
+            people.LName = lname;
+            people.Height = height;
+            people.Weight = weight;
 
 
 
 
-            return  new Person();
+            return people;
         }
 
-        //public void SetAge (Person pers, int age)
-        //{
-        //   pers.Age = age;
-        //}
-       
-        //public void SetHeight(Person pers, double height)
-        //{
-        //    pers.Height = height;
-        //}
-        //public void SetWeight(Person pers, double weight)
-        //{
-        //     pers.Weight = weight;
-        //}
-        //public void SetFname(Person pers, string fname)
-        //{
-        //    fname = pers.FName;
-        //}
-        //public void SetHeight(Person pers, string lname)
-        //{
-        //    lname = pers.LName;
-        //}
+
     }
 }
