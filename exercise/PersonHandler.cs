4f48706890cf8749d9082f71c4b25e44894f40f4
moshenahmed@gmail.com <moshenahmed@gmail.com>
2017-10-26 10:59:00 +0200

    first commit

---
+++ exercise/PersonHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise
+{
+    class PersonHandler
+    {
+        public Person CreatePerson(int age, string fname, string lname, double height, double weight)
+        {
+            Person people = new Person();
+            people.Age = age;
+            people.FName = fname;
+            people.LName = lname;
+            people.Height = height;
+            people.Weight = weight;
+
+
+
+
+            return people;
+        }
+    }
+}
