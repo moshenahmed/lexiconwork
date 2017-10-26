77357fdc6d3271373842f4266cb4555353104416
moshenahmed@gmail.com <moshenahmed@gmail.com>
2017-10-26 10:47:14 +0200

    first commit

---
+++ GroupWork/PersonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupWork
+{
+    class PersonHandler
+    {
+
+        public Person CreatePerson(int age, string fname, string lname, double height, double weight)
+        {
+            Person people = new Person();
+           people.Age = age;
+             people.FName = fname;
+             people.LName = lname;
+            people.Height = height;
+              people.Weight = weight;
+
+
+
+
+            return people;
+        }
+    }
+}
---
+++ GroupWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person();
+            PersonHandler Personone = new PersonHandler();
+
+ p1 =   Personone.CreatePerson(1, "jhdjdjd", "kdhdkjhd", 22.55, 66.66);
+            Console.WriteLine(p1.Age);
+        }
+
+       
+    }
+}
